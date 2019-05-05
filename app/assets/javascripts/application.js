bcda6c0d0542eb2119a74a31cf26d3c305909eab
Jerriyanto <jerryanto24@gmail.com>
2019-05-05 13:32:42 +0800

    Add fantasy foosball management into github 1. Ruby Version 2.4.2 2. Rails Version 5.2.3 3. Added bootstrap CSS and bootstrap Javascript 4. Added gem "activerecord-sqlserver-adapter"

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,28 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
+// vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file. JavaScript code in this file should be added after the last require_* statement.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require admin/core/jquery.min
+//= require admin/core/popper.min
+//= require admin/plugins/bootstrap-material-design.min
+//= require admin/plugins/perfect-scrollbar.jquery.min
+//= require admin/material-dashboard/material-dashboard
+//= require admin/material-dashboard/demo
+//= require admin/plugins/moment.min
+//= require admin/plugins/chartist.min
+//= require admin/plugins/arrive.min
+//= require admin/plugins/bootstrap-notify
+//= require rails-ujs
+
+
+//= require activestorage
+//= require turbolinks
+//= require_tree .
