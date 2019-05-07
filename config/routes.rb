632cf31735fddcf2f5140b8d95fc56c6a3c5b0a2
Jerriyanto <jerryanto24@gmail.com>
2019-05-07 18:44:15 +0800

    add "team" menu route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  
   root to: 'home#index'
   get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,6 +7,7 @@ Rails.application.routes.draw do
   #we route
   namespace :admin do
       resources :users
+      resources :teams
   end  
 
 end
